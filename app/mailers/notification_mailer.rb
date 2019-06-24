cfbccc1f2070c184296a579c7b75895eb60d1170
Enrique Cerna <ecerna96@gmail.com>
2019-06-24 23:49:38 +0000

    previewing email, currently emails not sent, will wait until mailing on production -lesson 41

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "ecerna96@gmail.com",
+    mail(to: "acerna943@gmail.com",
       subject: "A commnet has been added to your place")
   end
 end
